ec8dea612de11482aa2544e565ccf6b1c53c4161
Sameer Singh <sameersingh1449@gmail.com>
2025-07-13 10:17:41 +0530

    Making changes

--- docker.py
+++ docker.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/info")
 def lwinfo():
-    return "i am lw from indian"
+    return "i am lw from india"
 @app.route("/phone")
 def lwphone():
     return "8302666102"
